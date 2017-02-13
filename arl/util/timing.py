663f7532aa2f2d0e2250135fbd1135edf7edc812
Peter Wortmann <pw410@cam.ac.uk>
2017-02-13 13:51:08 +0000

    Add profiling decorator and object
    For quickly generating a profile for a function or block of code

--- arl/util/timing.py
+++ arl/util/timing.py
@@ -4,6 +4,7 @@ See comment by Matt Alcock at http://stackoverflow.com/questions/1622943/timeit-
 
 from functools import wraps
 from time import time
+import cProfile, pstats, io
 
 def timing(f, verbose=False):
     @wraps(f)
@@ -16,4 +17,30 @@ def timing(f, verbose=False):
         else:
             print('func:%r took: %2.4f sec' % (f.__name__, te - ts))
         return result
+    return wrap
+
+class Profiling(object):
+    """ Some quick profiling, as a block object """
+    def __init__(self, name='', keys=['cumulative'], restrictions=[10]):
+        self.name = name
+        self.keys = keys
+        self.restrictions = restrictions
+    def __enter__(self):
+        self.pr = cProfile.Profile()
+        self.pr.enable()
+    def __exit__(self, *args, **kw):
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(self.pr, stream=s).sort_stats(*self.keys)
+        ps.print_stats(*self.restrictions)
+        print('== Profile for %s' % self.name)
+        print(s.getvalue())
+
+def profiling(f, *pargs, **pkw):
+    """ Some quick profiling, as a function wrapper """
+    @wraps(f)
+    def wrap(*args, **kw):
+        with Profiling("func:%r args:[%r, %r]" % (f.__name__, args, kw),
+                       *pargs, **pkw):
+            return f(*args, **kw)
     return wrap
